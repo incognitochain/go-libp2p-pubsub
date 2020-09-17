d8f5800cbf030b29094b5f3f70c7e0be109e7856
0xakk0r0kamui <hy@autonomous.nyc>
2020-09-17 14:59:25 +0700

    fix bugs

--- trace.go
+++ trace.go
@@ -235,9 +235,6 @@ func (t *pubsubTracer) RecvRPC(rpc *RPC) {
 	}
 
 	now := time.Now().UnixNano()
-	// size := new(int64)
-	// if rpc.RPC.
-	// *size = int64(rpc.XXX_Size())
 	evt := &pb.TraceEvent{
 		Type:      pb.TraceEvent_RECV_RPC.Enum(),
 		PeerID:    []byte(t.pid),
@@ -245,7 +242,6 @@ func (t *pubsubTracer) RecvRPC(rpc *RPC) {
 		RecvRPC: &pb.TraceEvent_RecvRPC{
 			ReceivedFrom: []byte(rpc.from),
 			Meta:         t.traceRPCMeta(rpc),
-			// MsgSize:      0,
 		},
 	}
 
