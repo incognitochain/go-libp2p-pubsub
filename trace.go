400872e3a6a6417c3599d84d47f05fffe320b07f
0xakk0r0kamui <hy@autonomous.nyc>
2020-09-17 16:59:20 +0700

    Add msg size

--- trace.go
+++ trace.go
@@ -235,6 +235,8 @@ func (t *pubsubTracer) RecvRPC(rpc *RPC) {
 	}
 
 	now := time.Now().UnixNano()
+	size := new(int64)
+	*size = int64(rpc.XXX_Size())
 	evt := &pb.TraceEvent{
 		Type:      pb.TraceEvent_RECV_RPC.Enum(),
 		PeerID:    []byte(t.pid),
@@ -242,6 +244,7 @@ func (t *pubsubTracer) RecvRPC(rpc *RPC) {
 		RecvRPC: &pb.TraceEvent_RecvRPC{
 			ReceivedFrom: []byte(rpc.from),
 			Meta:         t.traceRPCMeta(rpc),
+			MsgSize:      size,
 		},
 	}
 
